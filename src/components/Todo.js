ee58d37235fadcdec674736ff7eabf2da9c79424
DESKTOP-CAGBJ2A\Duc Anh <anhhd.vnist@gmail.com>
2021-05-21 00:55:56 +0700

    lưu todo vào firestore

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,13 +13,13 @@ import Input from './Input';
 import Filter from './Filter';
 
 /* カスタムフック */
-import useStorage from '../hooks/storage';
+import useFbStorage from '../hooks/fbStorage';
 
 /* ライブラリ */
 import {getKey} from "../lib/util";
 
 function Todo() {
-    const [items, putItems, clearItems] = useStorage();
+    const [items, addItem, updateItem, clearItems] = useFbStorage();
     const [filter, setFilter] = React.useState('ALL');
 
     const displayItems = items.filter(item => {
@@ -28,17 +28,11 @@ function Todo() {
         if (filter === 'DONE') return item.done;
     });
     const handleCheck = checked => {
-        const newItems = items.map(item => {
-            if (item.key === checked.key) {
-                item.done = !item.done;
-            }
-            return item;
-        });
-        putItems(newItems);
+        updateItem(checked);
     };
 
     const handleAdd = text => {
-        putItems([...items, {key: getKey(), text, done: false}]);
+        addItem({ text, done: false });
     };
     const handleFilterChange = value => setFilter(value);
     return (
@@ -52,7 +46,7 @@ function Todo() {
                 value={filter}
             />
             {displayItems.map(item => (<TodoItem
-                    key={item.key}
+                    key={item.id}
                     item={item}
                     onCheck={handleCheck}
                 />
